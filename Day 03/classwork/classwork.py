706d55a7d246d32d5d7fc0599d8593fa0598d41b
ketalomtadze <ketalomtadze10@gmail.com>
2025-07-13 14:32:37 +0400

    commit

---
+++ Day 03/classwork/classwork.py
@@ -0,0 +1,17 @@
+print(type(20)) #integer
+print(type(12)) #integer
+print(type('hello')) #string
+print(type('111')) #integer
+print(type('1.5')) #string
+print(type(2.2)) #float
+print('Group 74') #string
+print('Hello world') #string
+
+name="keta"
+age=10
+print(name)
+print(age)
+name="alexandre"
+age=6
+print(name)
+print(age)
