7892c3bd2c02cf3b17d92b1d7cc3bc68d141e4bf
ketalomtadze <ketalomtadze10@gmail.com>
2025-08-26 13:20:12 +0400

    commit

---
+++ Day 17/classwork.py
@@ -0,0 +1,11 @@
+#რადგან გვიადვილებს კოდს და დაწერის დროს პირდაპირ პრინტს წერ
+
+if age == 10:
+    print("child discount")
+elif age == 64:
+    print("elderly discount")
+else:
+    print("regular price")
+
+
+#nested if ნიშნავს ჩაშენებულ იფს და შეგიძლა იფ
