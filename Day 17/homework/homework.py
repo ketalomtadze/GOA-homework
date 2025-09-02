2b50c8d371a57b714bde31fc2db3efd57f17f86b
ketalomtadze <ketalomtadze10@gmail.com>
2025-09-02 14:36:31 +0400

    commit

---
+++ Day 17/homework/homework.py
@@ -0,0 +1,79 @@
+age = int(input("გთხოვტ შეიყვანეთ თქვენი ასაკი"))
+
+if age <= 12:
+    print("child")
+elif age <= 19:
+    print("teenager")
+elif age <= 59:
+    print("adult")
+else:
+    print("pensioner")
+
+
+examscore = int(input("pls enter your score"))
+
+if examscore <=100:
+    print("A")
+elif   examscore <=89:
+    print("B")
+elif examscore <=79:
+    print("C")
+elif examscorescore <=69:
+    print("D")
+else:
+    print("F")
+
+
+number= int(input(pls enter a number))
+
+if number <=0:
+    print("negative") 
+elif number ==0:
+    print("zero")
+elif number <=100:
+    print("small number")
+else:
+    print("big number")
+
+
+day = int(input("enter a num"))
+
+if day == 1:
+    print("ორშ.")
+elif  day== 2:
+    print("სამშ.")
+elif day== 3:
+    print("ოთხშ.")
+elif day== 4:
+    print("ხუთშ.")
+elif day== 5:
+    print("პარ.")
+elif day== 6:
+    print("შაბ.")
+elif day== 7:
+    print("კვი.")
+
+
+temp = int(input("entera temperature num"))
+if temp <0:
+    print("it is very cold")
+elif temp <=15:
+    print("cool")
+elif temp <=30:
+    print("warm")
+elif temp <40:
+    print("hot")
+else:
+    print("it is extremely hot")
+
+
+
+
+
+
+
+
+
+
+
+
