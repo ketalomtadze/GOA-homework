9da15235af7765ad0df0c21d7ef90df3d1989ec4
ketalomtadze <ketalomtadze10@gmail.com>
2025-09-06 21:20:50 +0400

    commit

---
+++ Day 20/classwork/Day 19/classwork.py
@@ -0,0 +1,18 @@
+familiy=["mom-irine,dad-levani,me-keta,bro alexandre"]
+#print(familiy)
+
+#string,float,booleaninteger,list
+
+
+
+
+name="keta"
+age=10
+
+info=[name, age]
+print(info)
+
+
+#მეორე არ იმუშავებს
+
+#რადგან არის immutable
