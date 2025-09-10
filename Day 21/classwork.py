3d7ec3a996921476984a7360c1530201ea1ccefe
ketalomtadze <ketalomtadze10@gmail.com>
2025-09-11 00:07:41 +0400

    commit

---
+++ Day 21/classwork.py
@@ -0,0 +1,43 @@
+#0) შექმენით ლისთი, რომელშიც ჩამოწერთ 10 ელემენტს, შემდეგ გამოიტანეთ
+#პირველი ელემენტი positive indexingით, შემდეგ კი ბოლო ელემენტი negative indexingით
+
+names=["ana", "barbare", "giorgi", "dato", "elene", "vika", "zaza", "ia", "kato"]
+print(names[0])
+print(names[-1])
+
+
+#1) შექმენით ლისთი სახელად family, შემდეგ ამ ლისთის შიგნით შექმენით კიდევ რამდენიმე
+#ლისთი სადაც შეინახავთ თქვენი ოჯახის ინფორმციას
+
+family=[[4 "members"],["mom", "dad", "me","bro" ]]
+
+
+
+
+#2) ჩამოწერეთ ყველა slicing ტექნიკა, რომელიც ვისწავლეთ(გადახედეთ რესურსებს)
+#3)nums = [2, 4, 6, 8, 10, 12, 14, 16, 18]დავალება:slicing-ით აიღე პირველი ოთხი რიცხვი.slicing-ით აიღე შუა ოთხი რიცხვი.
+#slicing-ით მიიღე სია უკუღმა, მაგრამ ყოველ მეორე ელემენტზე.
+
+#Normal slicing,Negative slicing, continue to index, continue from index, skipping elements
+
+
+
+
+
+#nums = [2, 4, 6, 8, 10, 12, 14, 16, 18]
+#print(nums[0:3])
+
+#nums = [2, 4, 6, 8, 10, 12, 14, 16, 18]
+#print( nums[2:6])
+
+#nums = [2, 4, 6, 8, 10, 12, 14, 16, 18]
+#print(nums[::2])
+
+
+
+
+
+
+
+
+
